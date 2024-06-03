f7ed465474a3109f9dcd5f51aaf1715772eb9c15
fdervisi <fdervisi@googlemail.com>
2024-06-03 12:39:29 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
-  <NixFoundation msg="Welcome to the Lynn Nix Foundation" />
+  <NixFoundation msg="Welcome to the Fatos Nix Foundation" />
 </template>
 
 <script>
