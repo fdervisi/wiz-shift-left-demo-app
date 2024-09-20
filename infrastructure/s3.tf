a720bc1ffe5bc3b4a16723675fd89d5dd1fecf41
fdervisi <fdervisi@googlemail.com>
2024-09-20 07:08:41 +0000

    fix

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -17,8 +17,8 @@ resource "aws_s3_bucket" "default" {
 
 resource "aws_s3_bucket_public_access_block" "default" {
   bucket                  = aws_s3_bucket.default.id
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
